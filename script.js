7065a74910e9ca69a74d73ec7218055219b40076
Maryam-Nikpour <109689812+Maryam-Nikpour@users.noreply.github.com>
2023-11-20 10:50:21 -0500

    initial Project

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".nav-mobile-btn");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
+});
